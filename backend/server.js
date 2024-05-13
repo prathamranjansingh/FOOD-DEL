f7da708b921143a029361b886b9c039702ef6671
Pratham Ranjan Singh <67726405+prathamranjansingh@users.noreply.github.com>
2024-05-13 17:55:16 +0530

    userlogin and register update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
+import "dotenv/config";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
 // app config
 const app = express();
 const port = 4000;
@@ -16,6 +18,9 @@ connectDB();
 //api endpoints
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads")); //upload folder se access lega images ka
+
+app.use("/api/user", userRouter);
+
 app.get("/", (req, res) => {
   res.send("API Working");
 });
