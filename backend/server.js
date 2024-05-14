960f2fc7ad9d162cb61741bb816d6104f390fe22
Pratham Ranjan Singh <67726405+prathamranjansingh@users.noreply.github.com>
2024-05-14 17:42:47 +0530

    backend cart func

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
+import cartRouter from "./routes/cartRoute.js";
 // app config
 const app = express();
 const port = 4000;
@@ -18,8 +19,8 @@ connectDB();
 //api endpoints
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads")); //upload folder se access lega images ka
-
 app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
