ed3e3426888afc735fdba88db02621aaa4f0f68f
Pratham Ranjan Singh <67726405+prathamranjansingh@users.noreply.github.com>
2024-05-15 10:14:04 +0530

    Stripe integrated successfully

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -23,11 +23,11 @@ const placeOrder = async (req, res) => {
       price_data: {
         currency: "inr",
         product_data: {
-          name: item.name,
+          name: i.name,
         },
-        unit_amount: item.price * 100 * 80,
+        unit_amount: i.price * 100 * 80,
       },
-      quantity: item.quantity,
+      quantity: i.quantity,
     }));
 
     line_items.push({
@@ -36,7 +36,7 @@ const placeOrder = async (req, res) => {
         product_data: {
           name: "Delivery Charges",
         },
-        unit_amount: 2 * 100 * 80,
+        unit_amount: 2 * 100 * 80, //adding the delivery charges to the total amount
       },
       quantity: 1,
     });
@@ -47,7 +47,28 @@ const placeOrder = async (req, res) => {
       success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
       cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
     });
-  } catch (error) {}
+    res.json({ success: true, session_url: session.url });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
 };
 
-export { placeOrder };
+// we should use hooks rather than this
+const verifyOrder = async (req, res) => {
+  const { orderId, success } = req.body;
+  try {
+    if (success == "true") {
+      await orderModel.findByIdAndUpdate(orderId, { payment: true });
+      res.json({ success: true, message: "Paid" });
+    } else {
+      await orderModel.findByIdAndDelete(orderId);
+      res.json({ success: false, message: "Not Paid" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { placeOrder, verifyOrder };
