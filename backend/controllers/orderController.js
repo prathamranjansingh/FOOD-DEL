8b9adca8d5156d0bc77a7b4b7a5911690af74c24
Pratham Ranjan Singh <67726405+prathamranjansingh@users.noreply.github.com>
2024-05-16 01:37:43 +0530

    completed

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order from the frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "http://localhost:5174";
 
   try {
     const newOrder = new orderModel({
@@ -82,4 +82,29 @@ const userOrder = async (req, res) => {
   }
 };
 
-export { placeOrder, verifyOrder, userOrder };
+// Listing orders for admin pannel
+
+const listOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+//api for updating the order status
+const updateStatus = async (req, res) => {
+  try {
+    await orderModel.findByIdAndUpdate(req.body.orderId, {
+      status: req.body.status,
+    });
+    res.json({ success: true, message: "Status Updated" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { placeOrder, verifyOrder, userOrder, listOrders, updateStatus };
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -2,7 +2,9 @@ import express from "express";
 import authMiddleware from "../middleware/auth.js";
 
 import {
+  listOrders,
   placeOrder,
+  updateStatus,
   userOrder,
   verifyOrder,
 } from "../controllers/orderController.js";
@@ -12,4 +14,7 @@ const orderRouter = express.Router();
 orderRouter.post("/place", authMiddleware, placeOrder);
 orderRouter.post("/verify", verifyOrder);
 orderRouter.post("/userorders", authMiddleware, userOrder);
+orderRouter.get("/list", listOrders);
+orderRouter.post("/status", updateStatus);
+
 export default orderRouter;
